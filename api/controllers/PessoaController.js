10340ba54d26c62f1034d66ae326cc32c526e8c8
LeoST25 <matheusleonardo224@gmail.com>
2023-11-16 23:33:30 -0300

    Transações

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -192,6 +192,21 @@ static async pegaMatriculas(req, res) {
     }
   }
 
+  static async cancelaPessoa(req, res) {
+    const { estudanteId } = req.params
+    try {
+      database.sequelize.transaction(async transacao => {
+        await database.Pessoas
+          .update({ ativo: false }, { where: { id: Number(estudanteId) } }, { transaction: transacao })
+        await database.Matriculas
+          .update({ status: 'cancelado' }, { where: { estudante_id: Number (estudanteId) } }, { transaction: transacao })        
+        return res.status(200).json({ message: `matrículas ref. estudante ${estudanteId} canceladas` })
+      })
+      } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
 }
 
 module.exports = PessoaController
